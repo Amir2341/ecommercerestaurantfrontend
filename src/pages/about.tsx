4677ec867f160558a3ad06c2722d5d9533daf8cd
Amir <amirrafiq24@gmail.com>
2023-07-23 16:44:53 +0100

    updated about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,18 +10,14 @@ const About: FC = () => {
         <h1 className="text-7xl">About</h1>
       </div>
       <div className="flex flex-col justify-between items-start p-6 gap-5">
-        <div className="text-3xl">Good Food</div>
+        <div className="text-3xl">
+          Welcome to StackBurger - Your Fast Food Destination!
+        </div>
         <h2 className="text-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quas
-          possimus ipsam temporibus doloribus beatae saepe eum nesciunt dolorem
-          tempore, nemo dolores corporis officiis cumque neque veniam
-          distinctio, quae aperiam! Lorem, ipsum dolor sit amet consectetur
-          adipisicing elit. Vel provident error nemo optio sequi nostrum ab quis
-          consequuntur, est dolorum corrupti explicabo enim voluptas non quos
-          eligendi aliquid ipsum delectus! Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Asperiores quia inventore vitae labore
-          beatae veniam quam minima unde numquam similique non perferendis
-          voluptatum porro repellendus obcaecati, dolorum facilis itaque alias.
+          At StackBurger, we serve mouthwatering, high-quality meals with
+          lightning-fast service. From flame-grilled burgers to crispy chicken
+          nuggets and onion rings, there is something for everyone. Join us for
+          a delightful dining experience and discover fast food redefined!
         </h2>
       </div>
       <div className="flex flex-col justify-between items-start p-6 gap-5">
